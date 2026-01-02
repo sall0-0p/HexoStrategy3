6264dc5ebf9f422534b0aaa25bc12309697eb1b8
_lordBucket <antoniuk.orest@gmail.com>
2026-01-03 01:36:01 +0200

    Applying stuff in parallel, hope it works. heh.

--- SimLib/Core/System/JobSystemRunner.cs
+++ SimLib/Core/System/JobSystemRunner.cs
@@ -1,18 +1,25 @@
-using System.Collections.Concurrent;
 using Arch.Buffer;
 using Arch.Core;
 using SimLib.Core.System.Jobs;
-
 namespace SimLib.Core.System;
 
 public class JobSystemRunner : ISystemRunner
 {
     private readonly World _world;
     private readonly List<ISimulationSystem> _systems = [];
-    
+    private readonly CommandBuffer[] _threadBuffers;
+
     public JobSystemRunner(World world)
     {
         _world = world;
+        
+        var threadCount = Environment.ProcessorCount;
+        _threadBuffers = new CommandBuffer[threadCount];
+        
+        for (var i = 0; i < threadCount; i++)
+        {
+            _threadBuffers[i] = new CommandBuffer();
+        }
     }
     
     public void RegisterSystem(ISimulationSystem system)
@@ -23,54 +30,51 @@ public class JobSystemRunner : ISystemRunner
 
     public void RunTick(World world)
     {
-        var jobsToRun = new List<Jobs.ISystemJob>();
-
         foreach (var system in _systems)
         {
             if (system.ShouldRun())
             {
-                jobsToRun.Add(system.CreateJob());
+                RunJobParallel(system.CreateJob());
             }
         }
-
-        foreach (var job in jobsToRun)
+        
+        foreach (var buffer in _threadBuffers)
         {
-            RunJobParallel(job);
+            buffer.Playback(_world);
         }
     }
 
     private void RunJobParallel(ISystemJob job)
     {
-        var chunks = new List<Chunk>();
         var queryDesc = job.Query;
         var query = _world.Query(in queryDesc);
+        
+        var chunks = new List<Chunk>();
+        foreach (var chunk in query.GetChunkIterator())
+        {
+            chunks.Add(chunk);
+        }
 
-        foreach (var c in query.GetChunkIterator())
-        {
-            chunks.Add(c);
-        }
+        var totalChunks = chunks.Count;
+        if (totalChunks == 0) return;
         
-        var usedBuffers = new ConcurrentBag<CommandBuffer>();
-        Parallel.ForEach(
-            chunks, 
-            () => new CommandBuffer(), 
-            (chunk, loopState, localBuffer) => 
-            {
-                var context = new JobContext(Environment.CurrentManagedThreadId);
-                job.Execute(context, _world, localBuffer, chunk);
+        var threadCount = _threadBuffers.Length;
+        var batchSize = (totalChunks + threadCount - 1) / threadCount;
+        
+        Parallel.For(0, threadCount, threadIndex =>
+        {
+            var start = threadIndex * batchSize;
+            var end = Math.Min(start + batchSize, totalChunks);
             
-                return localBuffer;
-            },
+            if (start >= totalChunks) return;
+            var buffer = _threadBuffers[threadIndex];
             
-            (finalBuffer) => 
+            var context = new JobContext(threadIndex);
+            
+            for (var i = start; i < end; i++)
             {
-                usedBuffers.Add(finalBuffer);
+                job.Execute(context, _world, buffer, chunks[i]);
             }
-        );
-        
-        foreach (var buffer in usedBuffers)
-        {
-            buffer.Playback(_world);
-        }
+        });
     }
 }
